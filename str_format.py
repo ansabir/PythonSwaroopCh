629c2eb6a170636892bc6ae641c937bc48e81535
Sabir Ansari <ansabir95@gmail.com>
2021-02-19 15:15:18 +0530

    Learned about string formating

---
+++ str_format.py
@@ -0,0 +1,11 @@
+# Decalare variable like 'name', 'age', 'job' and companay with values respectively
+
+name = 'Sabir Ansari'
+age = 25
+job = 'Service Engineer'
+compnay = 'One Click Service'
+
+# Use format() with print() function to access the variable
+
+print('{0} is {1} years old'.format(name, age))
+print('{0} works in {1} as a {2}'.format(name, compnay, job))
