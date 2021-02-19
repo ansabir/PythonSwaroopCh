45aa8d16e5ebab2073b1ef939f5625dbd5288a85
Sabir Ansari <ansabir95@gmail.com>
2021-02-19 14:53:02 +0530

    add readme.md and run our first hello world program

---
+++ hello.py
@@ -0,0 +1,5 @@
+# below is the single line program which print the 'Hello World' to the console
+# after writing below program and in console type 'python hello.py
+#  print() is the function
+
+print('Hello World')
